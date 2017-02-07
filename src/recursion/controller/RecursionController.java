4bf762442c15d1dc07b5c53f0c5f5f9f4c2926a4
juvekeeper41 <josb6385@740s-pl-114.local>
2017-02-07 07:48:06 -0700

    Created and set up the recursion project.

---
+++ src/recursion/controller/RecursionController.java
@@ -0,0 +1,6 @@
+package recursion.controller;
+
+public class RecursionController 
+{
+
+}
---
+++ src/recursion/controller/RecursionRunner.java
@@ -0,0 +1,11 @@
+package recursion.controller;
+
+public class RecursionRunner 
+{
+	public static void main (String [] args)
+	{
+		RecursionController recursionAppController = new RecursionController();
+		recursionAppController.start();
+		
+	}
+}
