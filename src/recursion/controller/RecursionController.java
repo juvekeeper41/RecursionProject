991ff443515cb2298b012636c0eb5f98c2d26045
juvekeeper41 <josb6385@740s-pl-114.local>
2017-02-07 07:59:39 -0700

    Worked on the recursion classes.

--- src/recursion/controller/RecursionController.java
+++ src/recursion/controller/RecursionController.java
@@ -1,6 +1,13 @@
 package recursion.controller;
 
+import recursion.controller.RecursionController;
+
+
 public class RecursionController 
 {
 
+	public void start()
+	{
+		
+	}
 }
--- src/recursion/view/RecursionFrame.java
+++ src/recursion/view/RecursionFrame.java
@@ -1,6 +1,19 @@
 package recursion.view;
 
-public class RecursionFrame 
-{
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import recursion.controller.RecursionController;
+import recursion.view.RecursionPanel;
 
+public class RecursionFrame extends JFrame 
+{
+	private recursionPanel appPanel;
+	private RecursionController baseController;
+	
+	public recursionFrame(recursionController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		this.appPanel = new recursionPanel(baseController);
+	}
 }
